399066299bae6488d932f3bdb29ebe1cce510566
sergio <sergio.tjf1@gmail.com>
2022-05-22 21:17:53 -0300

    att2

--- script/script.js
+++ script/script.js
@@ -1,36 +1,26 @@
-/* SELEÇÃO NÚMERO */
+const numberBtn = document.querySelectorAll(".circle");
+const container = document.querySelector(".container");
+const containerThankYou = document.querySelector(".container-thankyou");
+const btnSubmit = document.querySelector("button");
+const span = document.querySelector(".rating");
 
-var numberBtn = document.querySelectorAll(".circle");
+for (let i = 0; i < numberBtn.length; i++) {
+  numberBtn[i].addEventListener("click", function () {
+    let rating = numberBtn[i].textContent;
+    console.log(rating);
 
-/* CLICK BOTÃO */
+    for (let i = 0; i < numberBtn.length; i++) {
+      numberBtn[i].classList.remove("circle2");
+      numberBtn[i].classList.add("circle");
+    }
+    numberBtn[i].classList.remove("circle");
+    numberBtn[i].classList.add("circle2");
 
-var btn = document
-  .querySelector("button")
-  .addEventListener("click", function () {
-    
-    var main = document.querySelector("main");
-
-    var container = document.querySelector(".container");
-    container.remove()
-
-    var div = document.createElement("div");
-    div.classList.add("Container2");
-    var span = document.createElement("p");
-    span.classList.add("spanTks")
-    var h1 = document.createElement("h1");
-    h1.classList.add("h1Tks");
-    var p = document.createElement("p");
-    p.classList.add("pTks");
-
-    span.textContent = "You selected out of 5";
-    h1.textContent = "Thank You!";
-    p.textContent = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
-
-    div.appendChild(span);
-    div.appendChild(h1);
-    div.appendChild(p);
-
-    main.appendChild(div);
-    
-    console.log(main)
+    btnSubmit.addEventListener("click", function () {
+      container.classList.add("remove");
+      containerThankYou.classList.remove("container-thankyou");
+      containerThankYou.classList.add("container-thankyou2");
+      span.textContent = "You selected" + rating + " out of 5";
+    });
   });
+}
